5b53842b4545f6c9ec4b65286f35b4115acf773b
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-09-13 18:26:41 +0100

    Use generated BURGRS logo in mobile header

--- src/components/BurgrsBanner.jsx
+++ src/components/BurgrsBanner.jsx
@@ -4,8 +4,8 @@ export default function BurgrsBanner() {
       <div className="burgrs-banner-overlay" />
       <div className="burgrs-banner-center">
         <img
-          src="/favicon-512.png"
-          alt="BURGRS"
+          src="/burgrs-header-logo.webp"
+          alt="BURGRS TV"
           className="burgrs-banner-burger"
           style={{
             position: "relative",
